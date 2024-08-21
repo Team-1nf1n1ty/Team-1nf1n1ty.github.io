1987416b493d534f98f502f8f43843ac022b3034
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-08-22 01:41:23 +0530

    Rules fix

---
+++ _nuxt/achievements.8c91cb66.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.f964f2d6.js";const _={};function n(t,c){return null}const r=e(_,[["render",n]]);export{r as default};
---
+++ _nuxt/this-route-is-for-developer-testing.86821d05.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.f964f2d6.js";const r={};function t(n,o){return null}const c=e(r,[["render",t]]);export{c as default};
